6800faffc6ebee52ecc8130a35054d0a2444892f
storres20 <sanchotorres777@gmail.com>
2022-04-16 02:11:17 -0500

    first try

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
 const API_URL = 'http://localhost:8080/api/products';
 
+const d = document,
+$table = d.querySelector(".crud-table"),
+$template = d.getElementById("crud-template").content,
+$fragment = d.createDocumentFragment();
+
 // Make a request for a user with a given ID
 axios.get(API_URL)
   .then(function (response) {
     console.log(response.data);
+    let data = response.data;
+    
+    data.forEach(el => {
+      $template.querySelector(".p_img").src = el.url_image;
+      $template.querySelector(".p_name").textContent = el.name;
+      $template.querySelector(".p_price").textContent = el.price;
+      
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+    
+    $table.appendChild($fragment);
+    
   })
   .catch(function (error) {
     console.log(error);
