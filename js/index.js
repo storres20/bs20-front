171ab1c6319e80816ba842ab5e61a7fef5da73dc
storres20 <sanchotorres777@gmail.com>
2022-04-16 23:05:13 -0500

    edit error response

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ const getAll = () => {
   document.getElementById("loader").style.display = "block";
   document.getElementById("crud-table").style.display = "none";
   document.querySelector("body").style.pointerEvents = "none";
-    
+
   // AXIOS - Get all data from product table and render in front
   axios.get(API_URL_PRODUCTS)
     .then(function (response) {
@@ -33,7 +33,7 @@ const getAll = () => {
         $template.querySelector(".p_name").textContent = el.name;
         $template.querySelector(".p_price").textContent = el.price;
         $template.querySelector(".p_price").style.textDecoration = (el.discount == 0) ? "" : "line-through"
-        
+
         $template.querySelector(".p_discount").textContent = el.price - el.discount;
 
         let $clone = d.importNode($template, true);
@@ -41,7 +41,7 @@ const getAll = () => {
       });
 
       $table.appendChild($fragment);
-      
+
       //Loader Off
       document.getElementById("loader").style.display = "none";
       document.getElementById("crud-table").style.display = "block";
@@ -73,20 +73,35 @@ const getAll = () => {
     })
     .catch(function (error) {
       console.log(error);
+      //Clean all
+      $table.innerHTML = "";
+
+      if (error.response.status == 500) {
+        document.querySelector(".titulofiltro").textContent = error.response.data.message;
+      }
+
+      //Loader Off
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("crud-table").style.display = "block";
+      document.querySelector("body").style.pointerEvents = "visible";
     });
 }
 
 d.addEventListener("DOMContentLoaded", getAll);
 
-// SUBMIT BTN - SEARCH BAR
+// SEARCH BAR
+/**
+ * Filtra los productos en base al nombre ingresar en el "Search Bar" y los muestra en pantalla.
+ * @param {string} el valor del "Search Bar" es enviado por AXIOS al Backend para el filtrado
+ */
 d.addEventListener("submit", e => {
   if (e.target === $form) {
     e.preventDefault();
     console.log(e.target.sb.value);
-    
+
     //Filter title
     document.querySelector(".titulofiltro").textContent = "Resultado de Busqueda";
-    
+
     //Loader On
     document.getElementById("loader").style.display = "block";
     document.getElementById("crud-table").style.display = "none";
@@ -94,7 +109,7 @@ d.addEventListener("submit", e => {
 
     axios.get(API_URL_PRODUCTS + "/search/" + e.target.sb.value)
       .then(function (response) {
-        console.log(response.data);
+        console.log(response);
         let datacat = response.data;
 
         //Clean all
@@ -105,9 +120,9 @@ d.addEventListener("submit", e => {
           $template.querySelector(".p_img").src = (el.url_image == "" || el.url_image === null) ? "./img/noimage.png" : el.url_image;
           $template.querySelector(".p_name").textContent = el.name;
           $template.querySelector(".p_price").textContent = el.price;
-          
+
           $template.querySelector(".p_price").style.textDecoration = (el.discount == 0) ? "" : "line-through"
-        
+
           $template.querySelector(".p_discount").textContent = el.price - el.discount;
 
           let $clone = d.importNode($template, true);
@@ -115,7 +130,7 @@ d.addEventListener("submit", e => {
         });
 
         $table.appendChild($fragment);
-        
+
         //Loader Off
         document.getElementById("loader").style.display = "none";
         document.getElementById("crud-table").style.display = "block";
@@ -125,6 +140,20 @@ d.addEventListener("submit", e => {
       })
       .catch(function (error) {
         console.log(error);
+        //Clean all
+        $table.innerHTML = "";
+
+        if (error.response.status == 404) {
+          document.querySelector(".titulofiltro").textContent = error.response.data.message;
+        }
+        else if (error.response.status == 500) {
+          document.querySelector(".titulofiltro").textContent = error.response.data.message;
+        }
+
+        //Loader Off
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("crud-table").style.display = "block";
+        document.querySelector("body").style.pointerEvents = "visible";
       });
 
   }
@@ -135,15 +164,15 @@ d.addEventListener("click", e => {
   if (e.target.matches(".s_name")) {
     console.log(e.target.dataset.id);
     console.log(e.target.dataset.name);
-    
+
     //Filter title
-    document.querySelector(".titulofiltro").innerHTML =  `<u>Filtrado por:</u> ` + e.target.dataset.name;
-    
+    document.querySelector(".titulofiltro").innerHTML = `<u>Filtrado por:</u> ` + e.target.dataset.name;
+
     //Loader On
     document.getElementById("loader").style.display = "block";
     document.getElementById("crud-table").style.display = "none";
     document.querySelector("body").style.pointerEvents = "none";
-    
+
     axios.get(API_URL_PRODUCTS + "/cat/" + e.target.dataset.id)
       .then(function (response) {
         console.log(response.data);
@@ -158,7 +187,7 @@ d.addEventListener("click", e => {
           $template.querySelector(".p_name").textContent = el.name;
           $template.querySelector(".p_price").textContent = el.price;
           $template.querySelector(".p_price").style.textDecoration = (el.discount == 0) ? "" : "line-through"
-        
+
           $template.querySelector(".p_discount").textContent = el.price - el.discount;
 
           let $clone = d.importNode($template, true);
@@ -166,7 +195,7 @@ d.addEventListener("click", e => {
         });
 
         $table.appendChild($fragment);
-        
+
         //Loader Off
         document.getElementById("loader").style.display = "none";
         document.getElementById("crud-table").style.display = "block";
@@ -176,6 +205,20 @@ d.addEventListener("click", e => {
       })
       .catch(function (error) {
         console.log(error);
+        //Clean all
+        $table.innerHTML = "";
+
+        if (error.response.status == 404) {
+          document.querySelector(".titulofiltro").textContent = error.response.data.message;
+        }
+        else if (error.response.status == 500) {
+          document.querySelector(".titulofiltro").textContent = error.response.data.message;
+        }
+
+        //Loader Off
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("crud-table").style.display = "block";
+        document.querySelector("body").style.pointerEvents = "visible";
       });
 
   }
